12e569618b1788096b13fb0ffaafc459eb40a951
MMPrado <maciaspradomayra@gmail.com>
2019-11-26 00:27:09 -0600

    Add files via upload

---
+++ main/java/mx/edu/utng/firebaseconkotlin/Clase.kt
@@ -0,0 +1,17 @@
+package mx.edu.utng.firebaseconkotlin
+
+class Clase(
+     val claseId: String? = null,
+     val carrera: String? = null,
+     val materia: String? = null,
+     val tema: String? = null
+
+
+)
+
+
+
+
+
+
+
